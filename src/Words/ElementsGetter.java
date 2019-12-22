fa9b45065948eac413567091974da4e6620cfdd6
foryourselfand <foryourselfand@gmail.com>
2019-12-22 21:39:13 +0300

    forgot

--- src/Words/ElementsGetter.java
+++ src/Words/ElementsGetter.java
@@ -5,32 +5,40 @@ import Utils.Resettable;
 public class ElementsGetter implements Resettable {
 	private ElementGetter[] elementGetters;
 	private IndexManipulator[] elementLengths;
+	private ElementFormatter elementsFormatter;
 	
-	public ElementsGetter(ElementGetter[] elementGetters, IndexManipulator[] elementLengths) {
+	public ElementsGetter(ElementGetter[] elementGetters, IndexManipulator[] elementLengths, ElementFormatter elementsFormatter) {
 		this.elementGetters = elementGetters;
 		this.elementLengths = elementLengths;
+		this.elementsFormatter = elementsFormatter;
+	}
+	
+	public ElementsGetter(ElementGetter[] elementGetters, IndexManipulator[] elementLengths) {
+		this(elementGetters, elementLengths, new ElementFormatter());
 	}
 	
 	public String getElements() {
 		assert this.elementGetters.length == this.elementLengths.length && this.elementGetters.length != 0;
 		
-		StringBuilder elements = new StringBuilder();
+		StringBuilder elementsBuilder = new StringBuilder();
 		for (int index = 0; index < this.elementGetters.length; index++) {
 			
 			int elementsLength = this.elementLengths[index].getIndex(this.elementGetters.length);
 			for (int tempLength = 0; tempLength < elementsLength; tempLength++) {
 				String elementTemp = this.elementGetters[index].getElement();
-				elements.append(elementTemp);
+				elementsBuilder.append(elementTemp);
 			}
 			elementGetters[index].reset();
 		}
-		return elements.toString();
+		String elements = elementsBuilder.toString();
+		elements = elementsFormatter.getFormattedElement(elementGetters.length, elements);
+		return elements;
 	}
 	
 	@Override
 	public void reset() {
 		assert this.elementGetters.length == this.elementLengths.length && this.elementGetters.length != 0;
-		for (int index = 0; index < elementGetters.length; index++){
+		for (int index = 0; index < elementGetters.length; index++) {
 			elementGetters[index].reset();
 			elementLengths[index].reset();
 		}
