58c93a8b9e5c8c01c08e0003e3e6fb23fb9cc616
foryourselfand <foryourselfand@gmail.com>
2019-12-22 11:29:28 +0300

    Condition Filters Sequence from many classes to one

--- src/Words/ConditionFilters/ConditionFilter.java
+++ src/Words/ConditionFilters/ConditionFilter.java
@@ -2,6 +2,8 @@ package Words.ConditionFilters;
 
 import Utils.RandomHolder;
 
+import java.util.Arrays;
+
 public abstract class ConditionFilter {
 	public abstract boolean condition(int index);
 	
@@ -117,6 +119,107 @@ public abstract class ConditionFilter {
 	public static class RemainderEqualsZero extends RemainderEqualsNumber {
 		public RemainderEqualsZero(int mod) {
 			super(mod, 0);
+		}
+	}
+	
+	public static abstract class SequenceFilter extends ConditionFilter {
+		protected int[] numbers;
+		
+		public SequenceFilter(int... numbers) {
+			if (numbers.length == 0)
+				throw new IllegalArgumentException();
+			this.numbers = numbers;
+			Arrays.sort(numbers);
+		}
+	}
+	
+	public static class NotEqualsFilter extends SequenceFilter {
+		public NotEqualsFilter(int... numbers) {
+			super(numbers);
+		}
+		
+		@Override
+		public boolean condition(int index) {
+			for (int number : numbers)
+				if (number == index)
+					return false;
+			return true;
+		}
+	}
+	
+	public static class EqualsFilter extends SequenceFilter {
+		public EqualsFilter(int... numbers) {
+			super(numbers);
+		}
+		
+		@Override
+		public boolean condition(int index) {
+			for (int number : numbers)
+				if (number == index)
+					return true;
+			return false;
+		}
+	}
+	
+	public static abstract class MinFilter extends SequenceFilter {
+		protected int minNumber;
+		
+		public MinFilter(int... numbers) {
+			super(numbers);
+			minNumber = numbers[0];
+		}
+	}
+	
+	public static class LessOrEqualsFilter extends MinFilter {
+		public LessOrEqualsFilter(int... numbers) {
+			super(numbers);
+		}
+		
+		@Override
+		public boolean condition(int index) {
+			return index <= minNumber;
+		}
+	}
+	
+	public static class LessFilter extends MinFilter {
+		public LessFilter(int... numbers) {
+			super(numbers);
+		}
+		
+		@Override
+		public boolean condition(int index) {
+			return index < minNumber;
+		}
+	}
+	
+	public static abstract class MaxFilter extends SequenceFilter {
+		protected int maxNumber;
+		
+		public MaxFilter(int... numbers) {
+			super(numbers);
+			maxNumber = numbers[numbers.length - 1];
+		}
+	}
+	
+	public static class GreaterFilter extends MaxFilter {
+		public GreaterFilter(int... numbers) {
+			super(numbers);
+		}
+		
+		@Override
+		public boolean condition(int index) {
+			return index > maxNumber;
+		}
+	}
+	
+	public static class GreaterOrEqualsFilter extends MaxFilter {
+		public GreaterOrEqualsFilter(int... numbers) {
+			super(numbers);
+		}
+		
+		@Override
+		public boolean condition(int index) {
+			return index >= maxNumber;
 		}
 	}
 	
